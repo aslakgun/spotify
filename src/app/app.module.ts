7f0dcd14e1bcd6c65e729473c9f632b87500d838
aslakgun <59713737+aslakgun@users.noreply.github.com>
2021-09-03 16:15:46 -0700

    Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { SpotifyServiceComponent } from './spotify-service/spotify-service.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatisticsComponent,
+    SpotifyServiceComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
